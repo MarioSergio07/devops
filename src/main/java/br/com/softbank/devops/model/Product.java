059f7edd1bb1c0c9f7cd3e432de870948b4df0d1
bruno.softbank@gmail.com <bruno.softbank@gmail.com>
2018-09-17 21:54:03 -0300

    Adicionado o plugin do sonar

--- src/main/java/br/com/softbank/devops/model/Product.java
+++ src/main/java/br/com/softbank/devops/model/Product.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "product")
 @Data
-public class Product {
+public class Product implements Comparable <Product>{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,4 +20,9 @@ public class Product {
     private String company;
     private String imagePath;
     private BigDecimal value;
+
+    @Override
+    public int compareTo(Product other) {
+        return this.name.compareTo(other.getName());
+    }
 }
